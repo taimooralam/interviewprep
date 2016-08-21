9d21390cca7883e32ab12fca0bcc5e6a485ea8ca
Taimoor Alam <taimoor.alam12@gmail.com>
2016-08-21 21:33:11 +0200

    Creates & prints a singly linked list

---
+++ concepts/linkedlists/1.struct-linedlist.c/1.struct-linkedlist.c.c
@@ -0,0 +1,44 @@
+//Made by Taimoor Alam
+//to run use gcc -o exe 1.\ struct-linkedlist\ c.c -std=c99
+#include <stdio.h>
+
+//The simplest useful singly linked list
+typedef struct IntElement {
+    struct IntElement *next;
+    int data;
+} IntElement;
+
+IntElement* createALinkedList(int n){
+    IntElement* head = NULL;
+    IntElement* prev = head;
+    for (int i = 0 ; i < n ; i++){
+        IntElement *element = (IntElement*)malloc(sizeof(IntElement));
+        element->data = i;
+        element->next = NULL;
+        if (i == 0){
+            head = element;
+            prev = element;
+            //printf("Value of head->data: %d\n",head->data);
+        }else{
+            //printf("Value of i: %d\n",i);
+            //printf("Value of prev->data: %d\n",prev->data);
+            prev->next = element;
+            prev = prev->next;
+        }
+    }
+    return head;
+}
+
+void printLinkedList(IntElement *head){
+    for (IntElement * pointer = head; pointer->next != NULL ; pointer=pointer->next){
+        printf("The element data is %d\n", pointer->data);
+    }
+}
+
+//the main function creates and prints a linkedlist
+void main(){
+    printf("This is the main function.\n");
+    IntElement* element = createALinkedList(10);
+    printLinkedList(element);
+    return;
+}
